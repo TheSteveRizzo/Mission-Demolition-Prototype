0f863d7081a7b9cf7ab77d8460e573fd44555c4a
Steve Rizzo <admin@steve-rizzo.com>
2020-10-13 13:49:07 -0400

    Added projectile particles
    Line now traces route of projectile launched by user

--- Assets/_Scripts/Slingshot.cs
+++ Assets/_Scripts/Slingshot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
+    
     [Header("Set In Inspector:")] public GameObject prefabProjectile;
     public float velocityMult = 8f;
 
@@ -14,8 +16,17 @@ public class Slingshot : MonoBehaviour
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
 
+    
+    static public Vector3 LAUNCH_POS {
+        get {
+            if (S == null ) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
