448a006828464eb1d35358cb121c0c3ca6c00249
Steve Rizzo <admin@steve-rizzo.com>
2020-10-14 15:44:31 -0400

    Added goal hit indicator
    Goal is light green when hit

---
+++ Assets/_Scripts/Goal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Goal : MonoBehaviour
+{
+    static public bool goalMet = false;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            Goal.goalMet = true;
+            Material mat = GetComponent<Renderer>().material;
+            mat.SetColor("_Color", Color.green);
+        }
+    }
+}
